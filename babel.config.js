55564b756b7f14ef0e83c5454cd9925080d8b468
kallsave <415034609@qq.com>
2020-07-10 12:41:15 +0800

    change: inline-source-map, babel plugins

--- babel.config.js
+++ babel.config.js
@@ -17,6 +17,8 @@ module.exports = {
     ]
   ],
   "plugins": [
+    ["@babel/plugin-proposal-decorators", { "legacy": true }],
+    ["@babel/plugin-proposal-class-properties", { "loose": true }],
     "@babel/plugin-syntax-dynamic-import",
     "@babel/plugin-transform-modules-commonjs"
   ],
